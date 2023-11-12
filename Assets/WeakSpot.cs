7f583bb5f2987fa145a646bfadaac90874cfde78
Virgil <virgile.fouche@gmail.com>
2023-11-12 14:01:48 +0100

    feature: add enemy and jump on their head to kill them

--- Assets/WeakSpot.cs
+++ Assets/WeakSpot.cs
@@ -6,6 +6,7 @@ public class WeakSpot : MonoBehaviour
     public GameObject objectToDestroy;
     void OnTriggerEnter2D(Collider2D collision)
     {
+        // destroy enemy when player collides with weak spot
         if (collision.gameObject.tag == "Player")
         {
             Destroy(objectToDestroy);
