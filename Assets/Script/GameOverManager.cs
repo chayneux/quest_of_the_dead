c98f0c449bd84ec69d1ecc0f9ba2b9e4b15d8165
chayneux <81871879+chayneux@users.noreply.github.com>
2023-12-04 17:05:30 +0100

    corrections + rajout changement scene

--- Assets/Script/GameOverManager.cs
+++ Assets/Script/GameOverManager.cs
@@ -24,7 +24,6 @@ public class GameOverManager : MonoBehaviour
 
     public void Retry()
     {
-        Debug.Log("retry");
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         gameOverUI.SetActive(false);
@@ -32,13 +31,11 @@ public class GameOverManager : MonoBehaviour
 
     public void Menu()
     {
-        Debug.Log("menu");
         SceneManager.LoadScene("MainMenu");
     }
 
     public void Quit()
     {
-        Debug.Log("Quit");
         Application.Quit(); 
     }
 }
---
+++ Assets/Script/NiveauSuivant.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class NiveauSuivant : MonoBehaviour
+{
+
+    private bool isInRange;
+    public TextMeshProUGUI  interactUI;
+    
+    void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            tag = "Player";
+        }
+        else if (GameObject.FindGameObjectWithTag("PlayerFantom") != null)
+        {
+            tag = "PlayerFantom";
+        }
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if(isInRange && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("intro");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(tag))
+        {
+            interactUI.enabled = true;
+            isInRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag(tag))
+        {
+            interactUI.enabled = false;
+            isInRange = false;
+        }
+        
+    }
+}
