48f7264d850fa7db9a4449a6ab25b19aee8ce79d
chayneux <81871879+chayneux@users.noreply.github.com>
2023-12-05 12:32:37 +0100

    rajout lvl 2 prototype

---
+++ Assets/openDoor.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+
+    public Animator animator; 
+
+    private string tag;
+
+    void Awake()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            tag = "Player";
+            Debug.Log("Player");
+        }
+        else if (GameObject.FindGameObjectWithTag("PlayerFantom") != null)
+        {
+            tag = "PlayerFantom";
+            Debug.Log("PlayerFantom");
+        }
+    }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Clef"))
+        {
+            animator.SetBool("isOpen", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Clef"))
+        {
+            animator.SetBool("isOpen", false);
+        }
+    }
+}
